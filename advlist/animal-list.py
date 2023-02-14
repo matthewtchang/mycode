16768cdf6fe98700b5fe67b245f45bd3722d8920
matthewtchang <matthewtchang25@gmail.com>
2023-02-14 23:17:55 +0000

    About complext lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+    # create animal list
+    animals = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+
+    print(animals)
+
+    main()
